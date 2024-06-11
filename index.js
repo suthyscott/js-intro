41378076f0558df053c951c49e6b0e2ae4faeb01
Scott Sutherland <sutherlandscott21@gmail.com>
2024-06-11 12:03:46 -0600

    added two files

---
+++ index.js
@@ -0,0 +1,99 @@
+// console.log('Hi')
+// console.log
+// console.log('How are you?')
+
+// const greeting = 'Hello, howdy!'
+
+// console.log(greeting)
+
+// let score = 0 
+// console.log(score)
+
+// score = 10
+// console.log(score)
+
+// let currentId
+
+// // after login
+// currentId = 76
+
+// console.log(currentId)
+
+// const scottsTanks = "scot's favorite tanks ..."
+
+// const lang = 'JavaScript'
+// const emphasis = '!!!'
+// const enthusiasm = ' is the best'
+
+// const empLang = lang + emphasis
+// const enthusiasticSentence = lang + enthusiasm + emphasis
+
+// console.log(enthusiasticSentence + 6)
+
+// const poem = `Roses are red
+// Sugar is sweet
+// His boots are too big
+// For his goshdarn feet`;
+// console.log(poem.length);
+
+// const adjective = 'tiny';
+// //                 0123
+// console.log(adjective[0])
+// const color = 'brown';
+// const animal = 'fox';
+
+// const conCatMadLib = 'The ' + adjective + ' ' + color + ' ' + animal + ' jumped over the lazy dog.'
+
+// const madlib = `The ${adjective} ${color} space hamster jumped over the lazy ${animal}.`
+// console.log(madlib);
+
+// const word = 'coffee' 
+
+// console.log(word.toUpperCase())
+
+// const total = (100 + .5) - 8
+
+// console.log(total)
+
+// let sum = 17
+
+// // sum = sum + 50
+// sum += 50
+// console.log(sum)
+// sum -= 4  
+// console.log(sum)
+
+
+// let sleepy = false
+
+// if(!sleepy) {
+//   console.log(`I'm not tired`)
+// } else {
+//   console.log('I need caffeine')
+// }
+
+// console.log( 201 > 200)
+
+// const myNumPets = 2;
+// const yourNumPets = 4;
+
+// if (myNumPets > yourNumPets) {
+//   console.log('I have more pets than you!');
+// } else if (myNumPets < yourNumPets) {
+//   console.log('You have more pets than me!');
+// } else if(myNumPets === 0 && yourNumPets === 0) {
+//   console.log("neither of us has pets")
+// } else {
+//   console.log('We have the same number of pets!');
+// }
+
+// const myPets = null 
+// let yourPets = undefined
+
+const giveMeFive = () => {
+    return 5
+  }
+  
+  console.log(giveMeFive())
+
+  console.log('hi world')
